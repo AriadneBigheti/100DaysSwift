1ee0d165181dbb26240d7c932f8ee76cb5d010ee
AriadneBigheti <83436908+AriadneBigheti@users.noreply.github.com>
2022-01-17 20:47:37 -0300

    day 33

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ariadne Bigheti on 17/01/22.
+//
+
+import Foundation
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ariadne Bigheti on 17/01/22.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
